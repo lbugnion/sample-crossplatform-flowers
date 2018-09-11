31b36e22c838ec5b5fc43d8179ade06de5297ea7
Laurent Bugnion <lbugnion@outlook.com>
2018-09-11 15:04:15 +0200

    Updated design flower with new URL

--- Flowers.Data/Design/DesignFlowersService.cs
+++ Flowers.Data/Design/DesignFlowersService.cs
@@ -34,7 +34,7 @@ namespace Flowers.Design
                 Name = "Flower # " + index,
                 Description =
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                Image = "http://www.galasoft.ch/labs/flowers/design/flowericon.png",
+                Image = "http://lbflowers.azurewebsites.net/design/flowericon.png",
                 Comments = new ObservableCollection<Comment>
                 {
                     new Comment
